29d316942448773df4400866638f3dab073347b8
Karina Jimenez <anakarina.h.j22@gmail.com>
2019-12-08 14:22:26 -0600

    Initial commit

---
+++ app/src/main/java/jimenez/karina/listview/ImagenesActivity.kt
@@ -0,0 +1,12 @@
+package jimenez.karina.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImagenesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imagenes)
+    }
+}
